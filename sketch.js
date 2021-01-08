5b2ae334fdbb1cb41b7790df2d95fc80c7ff9ceb
Mateen Kasim <mak420@pitt.edu>
2021-01-07 21:11:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let boids = [];
 let canvasWidth;
 let canvasHeight;
 const colors = ['red','blue','orange','green','yellow','magenta'];
-
+let qt;
 
 function setup() {
   canvasWidth = windowWidth;
@@ -11,14 +11,18 @@ function setup() {
   angleMode(DEGREES);
 
   // Initialize 
-  for (let i = 0; i < 150; i++) {
+  for (let i = 0; i < 400; i++) {
     boids.push(new Boid(Boid.viewRange*2*cos(i*5)+canvasWidth/2, Boid.viewRange*2*sin(i*5)+canvasHeight/2));
   }
 }
 
 function draw() {
+  qt = new Quadtree({x:0, y:0, width:canvasWidth, height:canvasHeight}, 10, 4);
   background('black');
-  boids.forEach(x => x.display());
+  boids.forEach(boid => {
+    boid.display();
+    qt.insert({x:boid.position.x-boid.diameter, y:boid.position.y-boid.diameter, width:boid.diameter*2, height:boid.diameter*2, b:boid});
+  });
   moveBoids();
 }
 
@@ -28,10 +32,12 @@ function mouseClicked() {
 
 function moveBoids() {
   boids.forEach(boid => {
-    let v1 = rule1(boid);
-    let v2 = rule2(boid);
-    let v3 = rule3(boid);
-    let v4 = rule4(boid);
+    let close = qt.retrieve({x:boid.position.x-boid.diameter, y:boid.position.y-boid.diameter, width:boid.diameter*2, height:boid.diameter*2, b:boid});
+
+    let v1 = rule1(boid, close);
+    let v2 = rule2(boid, close);
+    let v3 = rule3(boid, close);
+    let v4 = rule4(boid, close);
 
     boid.vel.add(v1);
     boid.vel.add(v2);
