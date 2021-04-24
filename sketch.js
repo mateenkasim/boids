772b9e3151c6393c753c6ab4920715c6f85987fa
Mateen Kasim <mak420@pitt.edu>
2021-04-24 17:06:52 -0400

    Add stars

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let boids = [];
 let canvasWidth;
 let canvasHeight;
-const colors = ['red','blue','orange','green','yellow','magenta'];
+const colors = ['red', 'blue', 'orange', 'green', 'yellow', 'magenta'];
 let qt;
 
 function setup() {
@@ -11,28 +11,55 @@ function setup() {
   angleMode(DEGREES);
 
   // Initialize 
-  for (let i = 0; i < 400; i++) {
-    boids.push(new Boid(Boid.viewRange*2*cos(i*5)+canvasWidth/2, Boid.viewRange*2*sin(i*5)+canvasHeight/2));
+  for (let i = 0; i < 240; i++) {
+    boids.push(new Boid(Boid.viewRange * 2 * cos(i * 5) + canvasWidth / 2, Boid.viewRange * 2 * sin(i * 5) + canvasHeight / 2));
   }
 }
 
 function draw() {
-  qt = new Quadtree({x:0, y:0, width:canvasWidth, height:canvasHeight}, 10, 4);
+  // Setup
+  qt = new Quadtree({
+    x: 0,
+    y: 0,
+    width: canvasWidth,
+    height: canvasHeight
+  }, 10, 4);
   background('black');
+  
+  // Stars
+  stroke('white');
+  randomSeed(1);
+  for (let i=0; i<250; i++)
+    point(random()*canvasWidth,random()*canvasHeight);
+  stroke('black');
+  
+  // Boids
   boids.forEach(boid => {
     boid.display();
-    qt.insert({x:boid.position.x-boid.diameter, y:boid.position.y-boid.diameter, width:boid.diameter*2, height:boid.diameter*2, b:boid});
+    qt.insert({
+      x: boid.position.x - boid.diameter,
+      y: boid.position.y - boid.diameter,
+      width: boid.diameter * 2,
+      height: boid.diameter * 2,
+      b: boid
+    });
   });
   moveBoids();
 }
 
 function mouseClicked() {
-  boids.push(new Boid(mouseX, mouseY, Math.random()*4-2, Math.random()*4-2));
+  boids.push(new Boid(mouseX, mouseY, Math.random() * 4 - 2, Math.random() * 4 - 2));
 }
 
 function moveBoids() {
   boids.forEach(boid => {
-    let close = qt.retrieve({x:boid.position.x-boid.diameter, y:boid.position.y-boid.diameter, width:boid.diameter*2, height:boid.diameter*2, b:boid});
+    let close = qt.retrieve({
+      x: boid.position.x - boid.diameter,
+      y: boid.position.y - boid.diameter,
+      width: boid.diameter * 2,
+      height: boid.diameter * 2,
+      b: boid
+    });
 
     let v1 = rule1(boid, close);
     let v2 = rule2(boid, close);
@@ -46,6 +73,6 @@ function moveBoids() {
   });
   boids.forEach(boid => {
     boid.limitVel();
-    boid.position.add(p5.Vector.div(boid.vel,1));
+    boid.position.add(p5.Vector.div(boid.vel, 1));
   });
 }
